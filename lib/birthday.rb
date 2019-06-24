a3483094c1fb431646c7bc75bbef2e721e068383
Adam Ervin <adamjervin@gmail.com>
2019-06-24 18:37:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
-  
-end
+  birthday_kids.each do |kname,age|
+    if age >= 12 
+      puts "Happy Birthday #{kname}! You are now #{age} years old!"
+  else 
