56a8b004f50293e734fd41ebcbf1354bb6f24bcc
Adam Ervin <adamjervin@gmail.com>
2019-06-24 18:33:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,14 +4,4 @@
 #   "Amanda" => 27
 # }
 
-def age_appropriate_birthday(birthday_kids)
- 
-age_appropriate_birthday.each do |kids_name,age| 
-  if age <= 12
-  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-else
-  
-end
-end
-
-end
+def 
